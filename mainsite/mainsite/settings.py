20c240451482f7dbd902bc5a88ac633c11f964f9
Gabriel Lacroix <lacroixgabriel@gmail.com>
2020-11-02 12:44:22 -0500

    Fix static and media files directories in settings

--- mainsite/mainsite/settings.py
+++ mainsite/mainsite/settings.py
@@ -114,8 +114,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "mysite", "site_static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "mysite", "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "mainsite", "site_static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "mainsite", "static")
 STATIC_URL = "/static/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "mysite", "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "mainsite", "media")
 MEDIA_URL = "/media/"
