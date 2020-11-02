d6ee41c26418c444062bca70355b24521c1f22b7
Gabriel Lacroix <lacroixgabriel@gmail.com>
2020-11-02 11:24:05 -0500

    Add basic views for Observations

--- mainsite/myapi/urls.py
+++ mainsite/myapi/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("targets/", views.TargetList.as_view()),
     path("targets/<int:pk>/", views.TargetDetail.as_view()),
+    path("observations/", views.ObservationList.as_view()),
+    path("observations/<int:pk>/", views.ObservationDetail.as_view()),
 ]
--- mainsite/myapi/views.py
+++ mainsite/myapi/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Target, Observation
 from .serializers import TargetSerializer, ObservationSerializer
+
 
 # Basic index response for a friendlier message when reaching the index
 def index(request):
@@ -25,3 +29,36 @@ class TargetDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Target.objects.all()
     serializer_class = TargetSerializer
+
+
+class ObservationList(APIView):
+    """
+    List all currently registered observations or create a new one.
+    """
+
+    # Needed to retrieve the image from the request
+    parser_classes = [MultiPartParser, FormParser]
+
+    # Return a list of all observations
+    def get(self, request, format=None):
+        observations = Observation.objects.all()
+        serializer = ObservationSerializer(targets, many=True)
+        return Response(serializer.data)
+
+    # Add a new observation
+    def post(self, request, format=None):
+        serializer = ObservationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ObservationDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Get, update or delete a specific observation using its primary key.
+    """
+
+    queryset = Observation.objects.all()
+    serializer_class = ObservationSerializer
