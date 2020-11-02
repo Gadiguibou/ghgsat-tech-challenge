96c755180e835f861bab3b48b66065a41adaec3d
Gabriel Lacroix <lacroixgabriel@gmail.com>
2020-11-02 11:18:07 -0500

    Add basic views for Target

--- mainsite/myapi/urls.py
+++ mainsite/myapi/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("targets/", views.TargetList.as_view()),
+    path("targets/<int:pk>/", views.TargetDetail.as_view()),
 ]
--- mainsite/myapi/views.py
+++ mainsite/myapi/views.py
@@ -1,5 +1,27 @@
 from django.http import HttpResponse
+from rest_framework import generics
 
+from .models import Target, Observation
+from .serializers import TargetSerializer, ObservationSerializer
 
+# Basic index response for a friendlier message when reaching the index
 def index(request):
     return HttpResponse("Hello from the index!")
+
+
+class TargetList(generics.ListCreateAPIView):
+    """
+    List all currently registered targets or create a new one.
+    """
+
+    queryset = Target.objects.all()
+    serializer_class = TargetSerializer
+
+
+class TargetDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Get, update or delete a specific target using its primary key.
+    """
+
+    queryset = Target.objects.all()
+    serializer_class = TargetSerializer
