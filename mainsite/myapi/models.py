cc0d24928407e20ebd972b26f6914de2325d9122
Gabriel Lacroix <lacroixgabriel@gmail.com>
2020-11-02 10:21:39 -0500

    Add Target model

--- mainsite/myapi/models.py
+++ mainsite/myapi/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Target(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+    latitude = models.DecimalField(
+        max_digits=8,
+        decimal_places=6,
+        validators=[MaxValueValidator(90), MinValueValidator(-90)],
+    )
+    longitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[MaxValueValidator(180), MinValueValidator(-180)],
+    )
